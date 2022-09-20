25811b6eccac0d05e8b4cd8cdfee03e48efeea2f
Raj1sahil <71655800+Raj1sahil@users.noreply.github.com>
2022-09-20 18:10:44 +0530

    Time: 791 ms (69.56%), Space: 31.7 MB (83.97%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,25 +1,28 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        dict1={}
+        f={}
         for i in arr:
-            if i in dict1:
-                dict1[i]=dict1[i]+1
+            if i in f:
+                f[i]=f[i]+1
             else:
-                dict1[i]=1
-
-        new_1=[]
-        for i,v in dict1.items():
-            new_1.append(v)
-
-        srt_1=sorted(new_1)
-        
+                f[i]=1
+            
+        d=[]
+        for i,v in f.items():
+            d.append(v)
+            
+        srt_1=sorted(d)
         
         size=len(arr)
         idmax=len(srt_1)-1
         count=0
         while size>len(arr)//2:
             size=size-srt_1[idmax]
-            idmax-=1
-            count+=1
-
+            idmax=idmax-1
+            count=count+1
         return count
+            
+        
+                
+        
+       
